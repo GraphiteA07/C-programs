56b372a7c5d5e10793d726ab5baab0163f5c384f
Zain <infopit327@gmail.com>
2024-03-03 09:56:37 +0500

    C-programs

--- Experiment.c
+++ Experiment.c
@@ -2,7 +2,30 @@
 
 int main(){
 	
-	printf("%lu", sizeof("main()"));
+	int num;
+	
+	printf("Enter a number\n");
+	scanf("%d", &num);
+	
+	printf("Your number is %d", num);
 	
 	return 0;
+
+	
 }
+
+
+
+/*	int Cyear,Byear;
+	int age = Cyear -Byear
+	
+	printf("Enter");
+	scanf("%d",&Cyear);
+	
+	printf("Enter your Birth year");
+	scanf("%d",&Byear);
+	
+	printf("Your age is %d",age);
+	
+	return 0; */ 
+
