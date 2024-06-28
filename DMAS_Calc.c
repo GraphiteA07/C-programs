628cd53874b79487583f9af501394ec511177019
Zain <infopit327@gmail.com>
2024-06-28 22:41:00 +0500

    10% Calculator complete

---
+++ DMAS_Calc.c
@@ -0,0 +1,693 @@
+/*
+
+                                  ---------  ABOUT THE PROGARM  ---------
+
+				  *The "_" keyword represents the function 
+				  * 
+
+
+
+
+
+ */ 
+#include <stdio.h>
+#include <unistd.h>
+
+    int delay= 1;       
+    int input_division;                   
+    int input_multiplication;
+    int input_addition;
+    int input_subtraction;                              
+    int input_square;
+    int input_cube;
+
+    
+
+
+
+
+    char choice;
+
+
+void sum_2() {
+
+  float a;
+  float b;
+
+
+  printf("\n\tEnter value 1 : ");
+  scanf("%f",&a);
+
+  printf("\n\tEnter value 2 : ");
+  scanf("%f",&b);
+
+  float adder2 = (a+b);
+
+  printf("\n\tOutput = %.2f",adder2);
+  } 
+
+
+void sum_3() {
+
+  float c;
+  float d;
+  float e;
+
+
+  printf("\n\tEnter value 1 : ");
+  scanf("%f",&c);
+
+  printf("\n\tEnter value 2 : ");
+  scanf("%f",&d);
+
+  printf("\n\tEnter value 3 : ");
+  scanf("%f",&e);
+
+  float adder3 = (c+d+e);
+
+  printf("\n\tOutput = %.2f",adder3);
+  } 
+
+
+void sum_4() {
+
+  float f;
+  float g;
+  float h;
+  float i;
+
+
+  printf("\n\tEnter value 1 : ");
+  scanf("%f",&f);
+
+  printf("\n\tEnter value 2 : ");
+  scanf("%f",&g);
+
+  printf("\n\tEnter value 3 : ");
+  scanf("%f",&h);
+
+  printf("\n\tEnter value 4 : ");
+  scanf("%f",&i);
+
+ 
+  float adder4 = (f+g+h+i);
+
+  printf("\n\tOutput = %.2f",adder4);
+  } 
+
+void sum_5() {
+
+  float j;
+  float k;
+  float l;
+  float m;
+  float n;
+
+
+  printf("\n\tEnter value 1 : ");
+  scanf("%f",&j);
+
+  printf("\n\tEnter value 2 : ");
+  scanf("%f",&k);
+
+  printf("\n\tEnter value 3 : ");
+  scanf("%f",&l);
+
+  printf("\n\tEnter value 4 : ");
+  scanf("%f",&m);
+
+  printf("\n\tEnter value 5 : ");
+  scanf("%f",&n);
+ 
+  float adder5 = (j+k+l+m+n);
+
+  printf("\n\tOutput = %.2f",adder5);
+  } 
+
+
+void sum_6() {
+
+  float o;
+  float p;
+  float q;
+  float r;
+  float s;
+  float t;
+
+
+  printf("\n\tEnter value 1 : ");
+  scanf("%f",&o);
+
+  printf("\n\tEnter value 2 : ");
+  scanf("%f",&p);
+
+  printf("\n\tEnter value 3 : ");
+  scanf("%f",&q);
+
+  printf("\n\tEnter value 4 : ");
+  scanf("%f",&r);
+
+  printf("\n\tEnter value 5 : ");
+  scanf("%f",&s);
+ 
+  printf("\n\tEnter value 6 : ");
+  scanf("%f",&t);
+
+  float adder6 = (o+p+q+r+s+t);
+
+  printf("\n\tOutput = %.2f",adder6);
+  } 
+
+
+void sum_7() {
+
+  float u;
+  float v;
+  float w;
+  float x;
+  float y;
+  float z;
+  float aa;
+
+
+  printf("\n\tEnter value 1 : ");
+  scanf("%f",&u);
+
+  printf("\n\tEnter value 2 : ");
+  scanf("%f",&v);
+
+  printf("\n\tEnter value 3 : ");
+  scanf("%f",&w);
+
+  printf("\n\tEnter value 4 : ");
+  scanf("%f",&x);
+
+  printf("\n\tEnter value 5 : ");
+  scanf("%f",&y);
+ 
+  printf("\n\tEnter value 6 : ");
+  scanf("%f",&z);
+
+  printf("\n\tEnter value 7 : ");
+  scanf("%f",&aa);
+
+  float adder7 = (u+v+w+x+y+z+aa);
+
+  printf("\n\tOutput = %.2f",adder7);
+  } 
+
+
+void sum_8() {
+
+  float ab;
+  float ac;
+  float ad;
+  float ae;
+  float af;
+  float ag;
+  float ah;
+  float ai;
+
+
+  printf("\n\tEnter value 1 : ");
+  scanf("%f",&ab);
+
+  printf("\n\tEnter value 2 : ");
+  scanf("%f",&ac);
+
+  printf("\n\tEnter value 3 : ");
+  scanf("%f",&ad);
+
+  printf("\n\tEnter value 4 : ");
+  scanf("%f",&ae);
+
+  printf("\n\tEnter value 5 : ");
+  scanf("%f",&af);
+ 
+  printf("\n\tEnter value 6 : ");
+  scanf("%f",&ag);
+
+  printf("\n\tEnter value 7 : ");
+  scanf("%f",&ah);
+  
+  printf("\n\tEnter value 8 : ");
+  scanf("%f",&ai);
+
+  float adder8 = (ab+ac+ad+ae+af+ag+ah+ai);
+
+  printf("\n\tOutput = %.2f",adder8);
+  }
+
+void sum_9() {
+
+  float aj;
+  float ak;
+  float al;
+  float am;
+  float an;
+  float ao;
+  float ap;
+  float aq;
+  float ar;
+
+
+  printf("\n\tEnter value 1 : ");
+  scanf("%f",&aj);
+
+  printf("\n\tEnter value 2 : ");
+  scanf("%f",&ak);
+
+  printf("\n\tEnter value 3 : ");
+  scanf("%f",&al);
+
+  printf("\n\tEnter value 4 : ");
+  scanf("%f",&am);
+
+  printf("\n\tEnter value 5 : ");
+  scanf("%f",&an);
+ 
+  printf("\n\tEnter value 6 : ");
+  scanf("%f",&ao);
+
+  printf("\n\tEnter value 7 : ");
+  scanf("%f",&ap);
+  
+  printf("\n\tEnter value 8 : ");
+  scanf("%f",&aq);
+
+  printf("\n\tEnter value 9 : ");
+  scanf("%f",&ar);
+  
+  float adder9 = (aj+ak+al+am+an+ao+ap+aq+ar);
+
+  printf("\n\tOutput = %.2f",adder9);
+  }
+
+
+void sum_10() {
+
+  float as;
+  float at;
+  float au;
+  float av;
+  float aw;
+  float ax;
+  float ay;
+  float az;
+  float ba;
+  float bb;
+
+
+  printf("\n\tEnter value 1 : ");
+  scanf("%f",&as);
+
+  printf("\n\tEnter value 2 : ");
+  scanf("%f",&at);
+
+  printf("\n\tEnter value 3 : ");
+  scanf("%f",&au);
+
+  printf("\n\tEnter value 4 : ");
+  scanf("%f",&av);
+
+  printf("\n\tEnter value 5 : ");
+  scanf("%f",&aw);
+ 
+  printf("\n\tEnter value 6 : ");
+  scanf("%f",&ax);
+
+  printf("\n\tEnter value 7 : ");
+  scanf("%f",&ay);
+  
+  printf("\n\tEnter value 8 : ");
+  scanf("%f",&az);
+
+  printf("\n\tEnter value 9 : ");
+  scanf("%f",&ba);
+  
+  printf("\n\tEnter value 10 : ");
+  scanf("%f",&bb);
+
+  float adder10 = (as+at+au+av+aw+ax+ay+az+ba+bb);
+
+  printf("\n\tOutput = %.2f",adder10);
+  }
+
+
+
+void sum_11() {
+
+  float bc;
+  float bd;
+  float be;
+  float bf;
+  float bg;
+  float bh;
+  float bi;
+  float bj;
+  float bk;
+  float bl;
+  float bm;
+
+
+  printf("\n\tEnter value 1 : ");
+  scanf("%f",&bc);
+
+  printf("\n\tEnter value 2 : ");
+  scanf("%f",&bd);
+
+  printf("\n\tEnter value 3 : ");
+  scanf("%f",&be);
+
+  printf("\n\tEnter value 4 : ");
+  scanf("%f",&bf);
+
+  printf("\n\tEnter value 5 : ");
+  scanf("%f",&bg);
+ 
+  printf("\n\tEnter value 6 : ");
+  scanf("%f",&bh);
+
+  printf("\n\tEnter value 7 : ");
+  scanf("%f",&bi);
+  
+  printf("\n\tEnter value 8 : ");
+  scanf("%f",&bj);
+
+  printf("\n\tEnter value 9 : ");
+  scanf("%f",&bk);
+  
+  printf("\n\tEnter value 10 : ");
+  scanf("%f",&bk);
+
+  printf("\n\tEnter value 11 : ");
+  scanf("%f",&bl);
+
+  float adder11 = (bc+bd+be+bf+bg+bh+bi+bj+bk+bl+bm);
+
+  printf("\n\tOutput = %.2f",adder11);
+
+  }
+
+
+void sum_12() {
+
+  float bn;
+  float bo;
+  float bp;
+  float bq;
+  float br;
+  float bs;
+  float bt;
+  float bu;
+  float bv;
+  float bw;
+  float bx;
+  float by;
+
+
+  printf("\n\tEnter value 1 : ");
+  scanf("%f",&bn);
+
+  printf("\n\tEnter value 2 : ");
+  scanf("%f",&bo);
+
+  printf("\n\tEnter value 3 : ");
+  scanf("%f",&bp);
+
+  printf("\n\tEnter value 4 : ");
+  scanf("%f",&bq);
+
+  printf("\n\tEnter value 5 : ");
+  scanf("%f",&br);
+ 
+  printf("\n\tEnter value 6 : ");
+  scanf("%f",&bs);
+
+  printf("\n\tEnter value 7 : ");
+  scanf("%f",&bt);
+  
+  printf("\n\tEnter value 8 : ");
+  scanf("%f",&bu);
+
+  printf("\n\tEnter value 9 : ");
+  scanf("%f",&bv);
+  
+  printf("\n\tEnter value 10 : ");
+  scanf("%f",&bw);
+
+  printf("\n\tEnter value 11 : ");
+  scanf("%f",&bx);
+
+  printf("\n\tEnter value 12 : ");
+  scanf("%f",&by);
+
+  float adder12 = (bn+bo+bp+bq+br+bs+bt+bu+bv+bw+bx+by);
+
+  printf("\n\tOutput = %.2f",adder12);
+
+  }
+int main() {
+
+ // int delay=1;
+ // char choice;
+
+  printf("\n\n\n\t\t\tDMAS CALCULATOR\n");
+  printf("\t\t============================\n\n\n");
+
+  printf("\n\tFor Division        ( d )\n");
+  printf("\n\tFor Multiplication  ( m )\n");
+  printf("\n\tFor Addition        ( a )\n");
+  printf("\n\tFor Subtraction     ( s )\n");
+  printf("\n\tFor Square          ( S )\n");
+  printf("\n\tFor Cube            ( c )\n");
+
+  
+  printf("\n\n\tWhat would it be :  ");
+  scanf("%c",&choice);
+
+
+   
+
+  if (choice =='d') { //parent bracket
+
+   printf("\n\tOperator : Division\n");
+
+      //int input_division; 
+      printf("\n\tHow many values: ");
+      scanf("%d",&input_division);
+
+
+   if (input_division == 1) {
+
+     sleep(delay);
+     printf("\n\tMinimun two values required.\n");
+
+   }
+
+   if (input_division == 2) {
+
+   }
+
+   if (input_division == 3) {
+
+   }
+
+  if (input_division == 3) {
+
+   }
+
+  if (input_division == 4) {
+
+   }
+
+  if (input_division == 5) {
+
+   }
+
+  if (input_division == 6) {
+
+   }
+
+  if (input_division == 7) {
+
+   }
+
+  if (input_division == 8) {
+
+   }
+
+  if (input_division == 9) {
+
+   }
+
+  if (input_division == 10) {
+
+   }
+
+  if (input_division == 11) {
+
+   }
+
+  if (input_division == 12) {
+
+   }
+
+
+  if (input_division == 13) {
+
+   }
+
+
+  if (input_division == 14) {
+
+   }
+
+
+  if (input_division == 15) {
+
+   }
+
+
+  if (input_division > 15) {
+
+      printf("\n\t.");
+      sleep (delay);
+
+      printf(".");
+      sleep (delay);
+
+      printf(".\n");
+      sleep (delay);
+
+      printf("\n\tMax limit reached! \n");
+   }
+
+
+  } //parent bracket of division
+
+
+
+   if (choice =='m') { //parent bracket
+
+   printf("\n\tOperator : Multiplication\n\n");
+
+  } //parent bracket of multiplication
+
+   
+if (choice !='d'&& choice !='m'&& choice !='a' && choice !='s'&& choice !='S'&& choice !='c') {
+
+      printf("\n\t.");
+      sleep (delay);
+
+      printf(".");
+      sleep (delay);
+
+      printf(".\n");
+      sleep (delay);
+
+      printf("\n\tOpreation : Unknown\n");
+
+      return 0;
+
+  }
+  
+  if (choice =='a') { //parent bracket
+
+   printf("\n\tOperator : Addition\n\n");
+
+  } //parent bracket of addition
+
+
+  if (choice =='s') { //parent bracket
+
+   printf("\n\tOperator : Subtraction\n\n");
+
+  } //parent bracket of subtraction
+
+
+  if (choice =='S') { //parent bracket
+
+   printf("\n\tOperator : Square\n\n");
+
+  } //parent bracket of square
+
+
+  
+  if (choice =='c') { //parent bracket
+
+   printf("\n\tOperator : Cube\n\n");
+
+  } //parent bracket of cube
+
+
+
+      //int input;
+
+
+
+      
+ 
+ /* switch (choice) {
+
+      case 'd':
+     
+
+      int input;
+
+      printf("\tHow many values: ");
+      scanf("%d",&input);
+
+      switch (input) {
+
+      case 1:
+      printf("\n\tMinimum two values required\n");
+      break;
+
+      case 2:
+      sum_2();
+      break;
+  }
+
+      case 'm':
+      printf("\n\tMultiplication\n");
+      break;
+
+      case 'a':
+      printf("\n\tAddition\n");
+      break;
+
+      case 's':
+      printf("\n\tSubtraction\n");
+      break;
+
+      case 'S':
+      printf("\n\tSquare\n ");
+      break;
+
+      case 'c':
+      printf("\n\tCube\n");
+      break;
+
+
+      default:
+      printf("\n\t.");
+      sleep (delay);
+
+      printf(".");
+      sleep (delay);
+
+      printf(".\n");
+      sleep (delay);
+
+      printf("\n\tOpreation not recognized!\n");
+      break;
+   
+
+  } */
+
+
+
+
+  return 0;
+}
