879615ab7538c838b34e70416486fa7f0f4cc953
Zain <infopit327@gmail.com>
2024-08-03 05:29:16 -0700

    Added programs

--- random.c
+++ random.c
@@ -1,52 +1,53 @@
-//Working Accordingly!
+//updated version of experiment.c
+//working accordingly!
 
 #include <stdio.h>
 #include <unistd.h>
 
 int main() {
 
-  char UserInput;
+  char userinput;
   char name[20];
   char c[4]= "yes";
-  char f[4]= "Yes";
+  char f[4]= "yes";
   char g[3]= "no";
-  char h[3]= "No";
+  char h[3]= "no";
  
   int e;
   int d;
 
 
-    printf("\n\tWhat's your name?\n\n");
+  printf("\n\twhat's your name?\n\n-> ");
   scanf("%s",&name);
 
-  printf("\nWelcome, %s \n",name);
+  printf("\nwelcome, %s \n",name);
 
-  printf("\n\tWant to know your age? (y/N)\n\n ");
-  scanf("%s",&UserInput);
+  printf("\n\twant to know your age? (y/n)\n\n-> ");
+  scanf("%s",&userinput);
 
 
 
-  if (UserInput=='Y' || UserInput=='y'|| UserInput=='c'|| UserInput=='f')  {
+  if (userinput=='y' || userinput=='y'|| userinput=='c'|| userinput=='f')  {
 
-     printf("\nLet's Begin!\n");
+     printf("\nlet's begin!\n");
     
-    printf("\n Enter the current year \n\t");
+    printf("\n enter the current year \n\n-> ");
     scanf("%d",&e);
 
-    printf("\n Enter your birth year \n\t");
+    printf("\n enter your birth year \n\n-> ");
     scanf("%d",&d);
 
     int c =(e-d);
 
-    printf("\n You are %d years old \n",c);
+    printf("\n you are %d years old! \n",c);
 
     return 0;
   }
 
-  
-  if (UserInput=='n'|| UserInput=='N'|| UserInput=='g'|| UserInput=='h') {
-    printf("\nOK");
 
+
+  if (userinput=='n'|| userinput=='n'|| userinput=='g'|| userinput=='h') {
+    printf("\nok");
   }
 
 
@@ -57,14 +58,43 @@ int main() {
 
     printf("\a\n");
 
-    printf("\tERROR:\n");
+    printf("\terror:\n");
     sleep(wait);
 
     printf("\a\a");
-    printf("\tBiryani coming from space!\n");
+    printf("\tbiryani coming from space!\n");
+  
 
   }
 
   return 0;
 
 }
+
+
+
+//logical errors
+
+
+//contains logic errors
+ /*{
+
+
+  char a[5] = "yes";
+  char b[5] = "no";
+  char c[5];
+
+  printf("want to know your age");
+  scanf("%s",&c);
+
+  if("c"=="a") {
+    printf("okay");
+
+
+  }
+  return 0; */
+
+
+
+ 
+   
