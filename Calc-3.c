85c59c55914ce3d703a4f25dfde7302a7baa5096
Zain <infopit327@gmail.com>
2024-09-12 18:29:00 -0700

    30% Done

--- Calc-3.c
+++ Calc-3.c
@@ -1,75 +1,293 @@
+//Has some interesting cases
+//Looping the OUTPUT on 'c'  --DONE--
+//Multiply function not multiplying ( WOW ) --DONE--
+//Subtract function not subtracting ( WOW ) 
+
+
 
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
+
+ int delay2 = 50000;   /*41900*/
+
+void ASCII_S() {
+
+
+printf("\n\n     8888b.  8b    d8    db    .dP\"Y8    dP\"\"b8    db    88      dP\"\"b8 \n");
+ usleep(delay2);
+printf("      8I  Yb 88b  d88   dPYb   `Ybo.\"   dP   `\"   dPYb   88     dP   `\" \n");
+ usleep(delay2);
+printf("      8I  dY 88YbdP88  dP__Yb  o.`Y8b   Yb       dP__Yb  88  .o Yb     \n");
+ usleep(delay2);
+printf("     8888Y\"  88 YY 88 dP\"\"\"\"Yb 8bodP'    YboodP dP\"\"\"\"Yb 88ood8  YboodP `  \n\n");
+printf("   _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_\n\n");
+}
+
+void ASCII() {
+
+printf("\n\n     8888b.  8b    d8    db    .dP\"Y8    dP\"\"b8    db    88      dP\"\"b8 \n");
+printf("      8I  Yb 88b  d88   dPYb   `Ybo.\"   dP   `\"   dPYb   88     dP   `\" \n");
+printf("      8I  dY 88YbdP88  dP__Yb  o.`Y8b   Yb       dP__Yb  88  .o Yb     \n");
+printf("     8888Y\"  88 YY 88 dP\"\"\"\"Yb 8bodP'    YboodP dP\"\"\"\"Yb 88ood8  YboodP `  \n\n");
+
+
+}
+
 
 int main() {
 
   system("cls");
   int delay = 1;
+  int OutputStore;
+  char option;
 
-
-  float mark[6];
-  float add = 0;
+  //for addition
   int i = 0;
-  /* int j = 0; */
-  /* int l = 2; */
-  /* int Loop2 = 2; */ 
-  /* int l = 0; */
+  float markA[10];
+  float add = 0;
 
-  while ( i < 6 ) {
+  //for multiplication
+  int j = 0;
+  float markM[10];
+  float mult = 1;
 
-  printf("\n Enter numbers : ");
-  scanf("%f",&mark[i]);
+  //for division
+  int k = 0;
+  float markD[10];
+  float div = 0;
 
-  if (mark[0] == 0 || mark[1] == 0) {
+  //for subtraction 
+  int l = 0;
+  float markS[10];
+  float sub = 0;
+  float SubOutput;
+ 
+ //for statement 
+  char AfterOutput;
+
+
+/* -_-_-_-_CODE STARTING POINT-_-_-_-_- */
+
+  do {
+
+  printf("\n\t  [ d ]   Division        \n");
+  printf("\n\t  [ m ]   Multiplication  \n");
+  printf("\n\t  [ a ]   Addition        \n");
+  printf("\n\t  [ s ]   Subtraction     \n");
+  printf("\n\t  [ S ]   Square          \n");
+  printf("\n\t  [ c ]   Cube            \n");
+  printf("\n\t  [ e ]   Exit            \n");
+
+  
+  printf("\n\n\tWhat would it be : ");
+  scanf("%s",&option);
+
+  switch ( option ) {
+
+
+    case 'a':
+
+  while ( i < 10 ) {
+
+  printf("\n\tEnter numbers : ");
+  scanf("%f",&markA[i]);
+
+  if (markA[0] == 0 || markA[1] == 0) {
 
     sleep(delay);
-    printf("\n Minimum 2 numbers required \n");
+    printf("\n\tMinimum 2 numbers required \n");
     return 0;
   }
 
-  else if ( mark[i] == 0 ) {
+   if ( markA[i] == 0 ) {
 
-      printf("\n OUPUT : %.3f \n",add);  
+  printf("\n--------------------------------------------------------------------");
+      printf("\n\n  OUPUT : %.3f \n",add);  
+  printf("\n--------------------------------------------------------------------\n");
+  i = 0;
+
+  add = 0;
       break;
     }
 
-    add += mark[i];
+    add += markA[i];
 
-
-  /* l++; */
   i++;
 
   }
+      break;
+
+
+    case 'm':
+
+  while ( j < 10 ) {
+
+  printf("\n\tEnter numbers : ");
+  scanf("%f",&markM[j]);
+
+  if (markM[0] == 0 || markM[1] == 0) {
+
+    sleep(delay);
+    printf("\n\tMinimum 2 numbers required \n");
+    return 0;
+  }
+
+   if ( markM[j] == 0 ) {
+
+  printf("\n--------------------------------------------------------------------");
+      printf("\n\n  OUPUT : %.3f \n",mult);  
+  printf("\n--------------------------------------------------------------------\n");
+  j = 0;
+
+  mult = 1;
+      break;
+    }
+
+    mult *= markM[j];
+
+
+  j++;
+
+  }
+      break;
+
+
+    case 's':
+
+  while ( l < 10 ) {
+
+  printf("\n\tEnter numbers : ");
+  scanf("%f",&markS[l]);
+
+  if (markS[0] == 0 || markS[1] == 0) {
+
+    sleep(delay);
+    printf("\n\tMinimum 2 numbers required \n");
+    return 0;
+  }
+
+   if ( markS[l] == 0 ) {
+
+  printf("\n--------------------------------------------------------------------");
+      printf("\n\n  OUPUT : %.3f \n",SubOutput);  
+  printf("\n--------------------------------------------------------------------\n");
+  l = 0;
+
+  sub = 0;
+      break;
+    }
+
+    sub += markS[l]; //sub was storing values with (-) when it was sub -= mark[i] Ex: -200 -100 = 300!
+    SubOutput = sub - markS[l-1]; //Having some errors
+
+
+  l++;
+
+  }
+  break;
+
+    case 'e':
+	printf("\n Exited Successfully!\n");
+	return 0;
+
+
+    default:
+	 printf("\n\t.");
+	 usleep(400000); 
+	 printf(".");
+	 usleep(500000); 
+	 printf(".\n");
+	 usleep(99999); 
+
+      printf("\n\tOperator : Unknown \n");
+      return 0;
+  }
+
+
    
-  printf("\n OK");
-     
+ printf("\n\n\n   WHERE TO PROCEED \n");
+ printf("==========================\n\n\n");
+
+    printf("\n Continue           ( c )\n");
+    printf("\n Exit               ( e )\n");
+
+    printf("\n Input : ");
+    scanf("%s",&AfterOutput);
+
+    while ( AfterOutput != 'c' && AfterOutput != 'e') {
+
+      printf("\n Enter 'c' or 'e' : ");
+      scanf("%1s",&AfterOutput);
+      scanf("%*[^\n]");
+
+    }
+
+    switch ( AfterOutput ) {
+
+      case 'c':
+
+    printf(" \nDirecting");
+    printf(".");
+    usleep(90000);
+    printf(".");
+    usleep(90000);
+    printf(".");
+    usleep(90000);
+
+
+
+   usleep(60000);
+    system("cls");
+    break;
+
+      case 'e':
+	printf("\n Exited Successfully!\n");
+	break;
+
+      default:
+	printf("\n Paji! Tusi kya likh rahe ro ji!\n");
+	break;
+    }
+
+
+  }
+
+  while ( AfterOutput == 'c' );
+
+
+/*   if ( AfterOutput == 'e') { */
+
+/*     printf(" \nExited Successfully!"); */
+/*     return 0; */
+/*   } */
+
+
+
+    /* switch ( AfterOutput ) { */
+
+    /*   case 'c': */
+	/* printf("\n True\n"); */
+	/* AfterOutput = add; */
+	/* break; */
+
+    /*   case 'e': */
+	/* printf("\n Exited\n"); */
+	/* break; */
+
+    /*   default: */
+	/* printf("\n Paji! Tusi kya likh rahe ro ji!\n"); */
+	/* break; */
+    /* } */
+
+
+
+
+
+
 
   return 0;
 }
 
 
 
-
-
-          //This loop didn't work
-    /* if (mark[Loop2] == 0 ) { */
-
-    /* printf("\n OUPUT : %.3f \n",add); */  
-    /* break; */
-
-    /* Loop2++; */
-
-    /* } */
-  
-  
-
-  /* while ( j < 6 ) { */
-
-
-  /*   printf("\n OUPUT : %d \n",add); */
-  /*   j++; */
-  
-
-    
-  /* } */
