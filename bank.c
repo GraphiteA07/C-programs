07f743afe6fc7e6c1755224bd743fc23d207113e
Zain <infopit327@gmail.com>
2024-10-07 01:32:49 -0700

    Verification stuff

--- bank.c
+++ bank.c
@@ -1,5 +1,6 @@
 //Password and name authentication are not completed
-//Add names fawad,talha,Muaz
+//Line 199 for user and password verification
+//New Name doesn't accept whitespaces in names
 
 #include <stdio.h>
 #include <string.h>
@@ -12,6 +13,9 @@ int main() {
 
 
   char Username[70];
+  char NewUsername[70];
+  char GivenPassword[10];
+  char UserPermission_AddSpecialCharacters; 
 
 
 
@@ -24,7 +28,12 @@ int main() {
     
   };
 
-  struct Users Ryan,Shayan,Ahmed,Khuzema,Fawad,Talha,Muaz;
+  struct Users User,Ryan,Shayan,Ahmed,Khuzema,Fawad,Talha,Muaz;
+
+  strcpy(User.Password, "password");
+  User.AccountBalance = 673573;
+
+
 
   Ryan.Found;
   Ryan.Loop = 0;
@@ -66,29 +75,16 @@ int main() {
   Muaz.AccountBalance = 657395;
 
 
-
-
-
-
   
-
-
- 
-
-
-
-
-
   char UserList[60][40] = {  "Ryan", "ryan", "RYAN", "Ryan Asif", "Ryan asif", "ryan Asif", "ryan asif", "RYAN ASIF",                                                 "Shayan","shayan","SHAYAN","Shayan Siddiqui","Shayan siddiqui","shayan Siddiqui","shayan siddiqui","SHAYAN SIDDIQUI",                    "Ahmed","ahmed","AHMED","Ahmed Dayan","Ahmed dayan","ahmed Dayan","ahmed dayan","AHMED DAYAN",                                          "Khuzema","khuzema","KHUZEMA","Khuzema Afridi","Khuzema afridi","khuzema Afridi","khuzema afridi","KHUZEMA AFRIDI",                      "Fawad","fawad","FAWAD","Fawad Ahmed","Fawad ahmed","fawad Ahmed","fawad ahmed","FAWAD AHMED",                                           "Talha","talha","TALHA","Talha Qadri","Talha qadri","talha Qadri","talha qadri","TALHA QADRI",                                            "Muaz","muaz","MUAZ","Muaz Khan","Muaz khan","muaz Khan","muaz khan","MUAZ KHAN",                                                                                                                                                                                                        };                
 			    
 
 
+  /* do { */
 
-
-
-     printf("\n Enter name \n\n > ");
+     printf("\n --Enter name-- \n\n > ");
      gets(Username);
-  
+
 
    while (Ryan.Loop < 8 ) {
 
@@ -195,17 +191,106 @@ int main() {
        Muaz.Loop++;
    }
 
-   if ( Ryan.Found || Shayan.Found || Ahmed.Found || Khuzema.Found || Fawad.Found || Talha.Found || Muaz.Found) {
 
-     printf("\n You are invited");
-     return 0;
-   }
+     printf("\n --Enter password-- \n\n > ");
+     scanf("%12s",&GivenPassword);
 
-   else {
+  //Checks for both Username and password
+  
+     if ( Ryan.Found && strcmp(Ryan.Password,GivenPassword)==0  ||  Shayan.Found && strcmp(Shayan.Password,GivenPassword)==0  ||  Ahmed.Found && strcmp(Ahmed.Password,GivenPassword)==0  ||  Khuzema.Found && strcmp(Khuzema.Password,GivenPassword)==0  ||  Fawad.Found && strcmp(Fawad.Password,GivenPassword)==0  ||  Talha.Found && strcmp(Talha.Password,GivenPassword)==0  ||  Muaz.Found && strcmp(Muaz.Password,GivenPassword)==0 /* ||  strcmp(User.Password,GivenPassword)==0*/) {
 
-     printf("\n Not invited");
-     return 0;
-   }
+       printf("\n Wow invited");
+       return 0;
+     }
+
+
+  //Tells the user to add special characters to name ( if name matches from UserList and password of "User.Password" is used )
+  
+     if ( Ryan.Found && strcmp(User.Password,GivenPassword)==0  ||  Shayan.Found && strcmp(User.Password,GivenPassword)==0  ||  Ahmed.Found && strcmp(User.Password,GivenPassword)==0  ||  Khuzema.Found && strcmp(User.Password,GivenPassword)==0  ||  Fawad.Found && strcmp(User.Password,GivenPassword)==0  ||  Talha.Found && strcmp(User.Password,GivenPassword)==0  ||  Muaz.Found && strcmp(User.Password,GivenPassword)==0 ) {
+
+       printf("\n There is a similar name in the Members List.Consider adding \n some special characters between full name or after full name.");
+       printf("\n\n [y] yes \n\n [n] no \n\n  > ");
+       scanf("%s",&UserPermission_AddSpecialCharacters);
+
+
+     }
+
+       while ( UserPermission_AddSpecialCharacters != 'y' && UserPermission_AddSpecialCharacters != 'n') {
+
+	 printf("\n Enter 'y' or 'n' : ");
+	 scanf("%s",&UserPermission_AddSpecialCharacters);
+	 scanf("%*[^\n]"); 
+       }
+
+
+       if (UserPermission_AddSpecialCharacters=='y') {
+
+
+	  printf("\n --Enter name-- \n\n > ");
+	  scanf("%s",&NewUsername);
+
+	  printf("\n New name : %s ",NewUsername);
+       }
+
+       if ( UserPermission_AddSpecialCharacters =='n') {
+	 return 0;
+       }
+
+
+
+
+  /* } */
+   /* while (UserPermission_AddSpecialCharacters=='y'); */
+
+
+
+
+
+     
+
+
+
+
+
+   /* if ( Ryan.Found || Shayan.Found || Ahmed.Found || Khuzema.Found || Fawad.Found || Talha.Found || Muaz.Found) { */
+
+   /*   printf("\n You are invited"); */
+   /*   return 0; */
+   /* } */
+
+   /* else { */
+
+   /*   printf("\n Not invited"); */
+   /*   return 0; */
+   /* } */
+
+
+  //Username and password are correct
+
+   /* if ( Ryan.Found && strcmp(Password,Ryan.Password) == 0 || Shayan.Found && strcmp(Password,Shayan.Password) == 0 || Ahmed.Found && strcmp(Password,Ahmed.Password) == 0 || Khuzema.Found && strcmp(Password,Khuzema.Password) == 0 || Fawad.Found && strcmp(Password,Fawad.Password) == 0 || Talha.Found && strcmp(Password,Talha.Password) == 0 || Muaz.Found && strcmp(Password,Muaz.Password) == 0 ) { */
+
+   /*   printf("\n WELCOME --%s--",Username); */
+   /*   return 0; */
+   /* } */
+
+
+
+   //If Username is on the UserList and Password is User.password
+   
+
+/*    while(User.Loop <55 ) { */
+
+/*    if ( strcmp(Username,UserList[User.Loop])==0 && strcmp(Password,User.Password) == 0 ) { */
+
+/*      printf("\n WELCOME --%s--",Username); */
+/*    } */
+
+/*    } */ 
+
+
+
+
+
  
 
   return 0;
